f5d09ba7f89022c26f26c47cc51de0f58883c9e4
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-05-23 17:26:41 +0900

    page5 curriculum

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def top
     @message = "Gatebookへようこそ！"
   end
+  # aboutアクションを追加してください
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/top'
+  # homeコントローラのaboutアクションへのルーティングを設定してください
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
