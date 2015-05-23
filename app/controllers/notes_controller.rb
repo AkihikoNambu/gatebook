3a306f4f34917441078ce23bcdbb0d051e0748c1
Akihiko Nambu <mate0rich.nmb@gmail.com>
2015-05-24 00:34:04 +0900

    page11 curriculum

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,7 +3,13 @@ class NotesController < ApplicationController
   end
 
   def create
-    @title = params[:title]
-    @content = params[:content]
+    # Noteクラスのインスタンスを生成し、@noteに代入してください
+
+    # セッターを用いて、params[:title]をセットしてください
+
+    # セッターを用いて、params[:content]をセットしてください
+
+    # saveメソッドを呼び出して、データベースに保存してください
+
   end
 end
